ecf31c4e51c68864794c6dfd4ff103e3aafe318b
ChrisCox12 <chriscox9856@gmail.com>
2022-06-29 22:09:28 -0400

    updated axios util

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -2,19 +2,22 @@ import axios from 'axios';
 
 
 const REACT_APP_API_URL = process.env.REACT_APP_API_URL;
-let headers = {};
-
-
-if(localStorage.getItem('audiophile_admin_token')) {
-    //  sets authorization header for token authentication
-    headers.authorization = `Bearer ${localStorage.getItem('audiophile_admin_token')}`;
-}
-
 
 //  creates an axios instance that can be used across the application
-const axiosInstance = axios.create({
-    baseURL: REACT_APP_API_URL,
-    headers: headers
-});
+const axiosInstance = axios.create({ baseURL: REACT_APP_API_URL });
+
+//  by using interceptors, ensures that authorization header is set before every request
+axiosInstance.interceptors.request.use(
+    function(config) {
+        if(localStorage.getItem('audiophile_admin_token')) {
+            config.headers['authorization'] = `Bearer ${localStorage.getItem('audiophile_admin_token')}`;
+        }
+        
+        return config;
+    }, 
+    function(error) {
+        return Promise.reject(error);
+    }
+);
 
 export default axiosInstance;
